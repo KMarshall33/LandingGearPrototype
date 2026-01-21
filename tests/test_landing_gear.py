b9e23688aee3ad9d1ed99e71a6b82d900eea3012
KMarshall33 <30208892@blackpool.ac.uk>
2026-01-21 21:22:54 +0000

    Expand test for tick-based deployment behaviour

--- tests/test_landing_gear.py
+++ tests/test_landing_gear.py
@@ -4,7 +4,23 @@ def test_initial_state_is_up_locked():
   controller = LandingGearController()
   assert controller.state == GearState.UP_LOCKED
 
-def test_command_gear_down():
+def test_command_gear_down_enters_transition():
   controller = LandingGearController()
   controller.command_gear_down()
+  assert controller.state == GearState.TRANSITIONING_DOWN
+
+def test_gear_does_not_lock_down_immediately():
+  controller = LandingGearController()
+  controller.command_gear_down()
+  controller.tick()
+  assert controller.state == GearState.TRANSITIONING_DOWN
+
+def test_gear_locks_after_three_ticks():
+  controller = LandingGearController()
+  controller.command_gear_down()
+
+  controller.tick()
+  controller.tick()
+  controller.tick()
+  
   assert controller.state == GearState.DOWN_LOCKED
