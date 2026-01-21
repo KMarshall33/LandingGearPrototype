b527319aa55727b46db6b9d01d9b9f66a70b18c8
KMarshall33 <30208892@blackpool.ac.uk>
2026-01-21 11:21:53 +0000

    Add baseline tests

--- tests/test_landing_gear.py
+++ tests/test_landing_gear.py
@@ -1,2 +1,10 @@
-def test_landing_gear():
-  assert True
+from LandingGear import LandingGearController, GearState
+
+def test_initial_state_is_up_locked():
+  controller = LandingGearController()
+  assert controller.state == GearState.UP_LOCKED
+
+def test_command_gear_down():
+  controller = LandingGearController()
+  controller.command_gear_down()
+  assert controller.state == GearState.DOWN_LOCKED
