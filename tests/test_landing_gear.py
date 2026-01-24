9b3f515e7fd21803375dd64ef4a2c4252a92fa28
KMarshall33 <30208892@blackpool.ac.uk>
2026-01-24 19:27:17 +0000

    Expand test coverage

--- tests/test_landing_gear.py
+++ tests/test_landing_gear.py
@@ -1,4 +1,4 @@
-from landing_gear import LandingGearController, GearState
+from landing_gear import LandingGearController, GearState, DEPLOY_TICKS
 
 def test_initial_state_is_up_locked():
   controller = LandingGearController()
@@ -36,3 +36,44 @@ def test_command_gear_down_is_accepted_when_hydraulic_pressure_ok():
   controller.hydraulic_pressure_ok = True
   controller.command_gear_down()
   assert controller.state == GearState.TRANSITIONING_DOWN
+
+def test_tick_does_nothing_when_not_transitioning():
+  controller = LandingGearController()
+  controller.tick()
+  assert controller.state == GearState.UP_LOCKED
+
+def test_command_gear_down_when_already_down_locked():
+  controller = LandingGearController()
+  controller.command_gear_down()
+  for _ in range(DEPLOY_TICKS):
+    controller.tick()
+  assert controller.state == GearState.DOWN_LOCKED
+
+  controller.command_gear_down()
+  assert controller.state == GearState.DOWN_LOCKED
+
+def test_duplicate_command_gear_down_during_transition():
+  controller = LandingGearController()
+  controller.command_gear_down()
+
+  controller.tick()
+  assert controller.state == GearState.TRANSITIONING_DOWN
+
+  controller.command_gear_down()
+
+  for _ in range(DEPLOY_TICKS - 1):
+    controller.tick()
+
+  assert controller.state == GearState.DOWN_LOCKED
+
+def test_rejected_command_does_not_transition_or_tick():
+  controller = LandingGearController()
+  controller.hydraulic_pressure_ok = False
+
+  controller.command_gear_down()
+  assert controller.state == GearState.UP_LOCKED
+
+  for _ in range(DEPLOY_TICKS + 1):
+    controller.tick()
+
+  assert controller.state == GearState.UP_LOCKED
